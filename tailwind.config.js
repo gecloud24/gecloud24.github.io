6c9e35ac701e70816dc522edc6b8e195a992f3f9
Yunfei.Ge <geyf1@chinatelecom.cn>
2026-06-24 12:08:43 +0800

    Polish homepage assets and build CSS

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', '"Noto Sans SC"', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          50: '#f0f9ff',
+          100: '#e0f2fe',
+          600: '#0284c7',
+          700: '#0369a1',
+          900: '#0c4a6e',
+        },
+        telecom: {
+          blue: '#0056b3',
+        },
+      },
+    },
+  },
+};
